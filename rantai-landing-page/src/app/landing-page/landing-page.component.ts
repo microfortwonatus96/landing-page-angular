b4de64dd2f08d3d3a843f0fe754861a7d025f0aa
Teddy Pratama <teddy.pratama0123@gmail.com>
2020-11-09 03:05:27 -0800

    progress responsive

--- rantai-landing-page/src/app/landing-page/landing-page.component.ts
+++ rantai-landing-page/src/app/landing-page/landing-page.component.ts
@@ -10,6 +10,14 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    let scrollXCenter = () => {
+      let overview = document.querySelector("#overview > div");
+      if (window.innerWidth <= 1440) {
+        overview.scrollLeft = (overview.scrollWidth - window.innerWidth) / 2;
+      }
+    };
+    window.addEventListener("resize", scrollXCenter);
+    window.addEventListener("load", scrollXCenter);
   }
 
 }
