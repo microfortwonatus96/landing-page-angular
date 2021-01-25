4783b4b0838021e9b4bffc24282f1a3387b71592
yusuf <yusuf@cyberindo-sinergi.com>
2021-01-25 14:16:31 +0700

    update new landing page

--- rantai-landing-page/src/app/landing-page/landing-page.component.ts
+++ rantai-landing-page/src/app/landing-page/landing-page.component.ts
@@ -1,37 +1,37 @@
 import { registerLocaleData } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
   templateUrl: './landing-page.component.html',
-  styleUrls: ['./landing-page.component.css']
+  styleUrls: ['./landing-page.component.css'],
 })
 export class LandingPageComponent implements OnInit {
-  show = true
+  show = true;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     let scrollXCenter = () => {
-      let overview = document.querySelector("#overview > div");
-      let feature = document.querySelector("#feature > div");
+      let overview = document.querySelector('#overview > div');
+      let feature = document.querySelector('#feature > div');
       if (window.innerWidth <= 1440) {
         overview.scrollLeft = (overview.scrollWidth - window.innerWidth) / 2;
         feature.scrollLeft = (feature.scrollWidth - window.innerWidth) / 2;
       }
     };
-    window.addEventListener("resize", scrollXCenter);
-    window.addEventListener("load", scrollXCenter);
+    window.addEventListener('resize', scrollXCenter);
+    window.addEventListener('load', scrollXCenter);
   }
 
   redirect() {
-    window.location.href = "https://play.google.com/store/apps/details?id=com.css.rantai";
+    window.location.href =
+      'https://play.google.com/store/apps/details?id=com.css.rantai';
   }
 
   jumpto(value: string) {
     var element = document.getElementById(value);
     var offset = element.getBoundingClientRect().top - 20;
-    window.scrollBy({ top: offset, behavior: "smooth" });
+    window.scrollBy({ top: offset, behavior: 'smooth' });
   }
-
 }
