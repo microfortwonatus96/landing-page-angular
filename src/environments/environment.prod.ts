7e1a042163cc0f7b308a833e20bc1775e79edf17
Steven <steven@cyberindo-sinergi.com>
2022-01-28 11:53:29 +0700

    fix package name

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,7 @@ export const environment = {
   api_server: 'https://rantai.tech/app',
   api_support: 'https://rantailink.com',
   api_referral: 'https://rantailink.com',
+  api_galery: 'https://rantai-marketplace.com/storage/api',
+
   production: true,
 };
